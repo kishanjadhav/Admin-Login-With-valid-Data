a9e745f924ca2a6635ed343f22147c0b2119fc0d
QEdge Lab5 <QEdge Lab5@DESKTOP-QNFJPCH>
2022-06-16 11:35:21 +0530

    commit New Hybrid

---
+++ apachepoi/src/PRIMITIVE.java
@@ -0,0 +1,30 @@
+
+public class PRIMITIVE {
+
+	public static void main(String[] args) {
+		// iNTEGER type
+		byte b=12;
+		System.out.println(" byte is::"+b);
+		short s=45;
+		System.out.println("short is::"+s);
+		int i=200;
+		System.out.println("integer is::"+i);
+		long l=2000;
+		System.out.println("long is ::"+l);
+		//floating or decimal type
+	    float f=200;
+	    System.out.println("float is ::"+f);
+	    double d=1000;
+	    System.out.println("double is::"+d);
+	    // characters type
+	    char c=500;
+	    System.out.println("character is::"+c);
+	    // conditional type
+	    boolean t=true;
+	    System.out.println("boolean is::"+t);
+	
+				
+		 
+	}
+
+}
